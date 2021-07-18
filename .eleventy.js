98e9fc468f554c2abdfaa86f338bf9537826ad01
Eisenbarth <oliver.eisenbarth@gmail.com>
2021-07-18 18:19:36 +0200

    Fix: Add missing files

---
+++ .eleventy.js
@@ -0,0 +1,30 @@
+const sortByDisplayOrder = require('./src/utils/sort-by-display-order.js');
+
+module.exports = config => {
+
+    // Set directories to pass through to the dist folder
+    config.addPassthroughCopy('./src/images/');
+
+    // Returns work items, sorted by display order
+    config.addCollection('work', collection => {
+        return sortByDisplayOrder(collection.getFilteredByGlob('./src/work/*.md'));
+    });
+
+    // Returns work items, sorted by display order then filtered by featured
+    config.addCollection('featuredWork', collection => {
+        return sortByDisplayOrder(collection.getFilteredByGlob('./src/work/*.md')).filter(
+            x => x.data.featured
+        );
+    });
+
+    return {
+        // process markdown-, data- and html-files with nunjucksmkdir -p src/_includes/layouts
+        markdownTemplateEngine: 'njk',
+        dataTemplateEngine: 'njk',
+        htmlTemplateEngine: 'njk',
+        dir: {
+            input: 'src',
+            output: 'dist'
+        }
+    };
+};
